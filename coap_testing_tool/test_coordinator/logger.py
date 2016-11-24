1d239c528cad3d0c007400be9968bcdfd8b3890e
Federico Sismondi <federicosismondi@gmail.com>
2016-11-24 18:20:47 +0100

    log handlers

--- coap_testing_tool/test_coordinator/logger.py
+++ coap_testing_tool/test_coordinator/logger.py
@@ -1,9 +1,9 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+
 import logging
 import os.path
-from http.server import BaseHTTPRequestHandler, HTTPServer
 
-COMPONENT_DIR = 'coap_testing_tool/test_coordinator'
-LOGDIR = os.path.join( os.getcwd(), COMPONENT_DIR, 'log')
 
 
 def initialize_logger(output_dir):
@@ -31,42 +31,5 @@ def initialize_logger(output_dir):
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
-
-class HTTPServer_RequestHandler(BaseHTTPRequestHandler):
-    # GET
-    def do_GET(self):
-        # Send response status code
-        self.send_response(200)
-
-        # Send headers
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-
-        with open(os.path.join(LOGDIR,'all.log')) as f:
-            cr = '<br/>'
-            for line in f:
-                # write the contents as bytes
-                self.wfile.write(bytes(line, 'utf-8'))
-                # write the contents as bytes
-                self.wfile.write(bytes(cr, 'utf-8'))
-
-
-
-
-
-
-        f.close()
-        return
-
-        # # Send message back to client
-        # with open(os.path.join(LOGDIR,'all.log'), "r") as file:
-        #     read_data = file.read()
-        #
-        #
-        #
-        #     # Write content as utf-8 data
-        #     self.wfile.write(bytes(read_data),'utf-8')
-        #     self.wfile.flush()
-        #     return
 
 
