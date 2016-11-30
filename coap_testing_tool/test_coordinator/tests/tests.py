d2f3fcc916fe3fc76035bb390d5a10d3eb778be4
Federico Sismondi <federicosismondi@gmail.com>
2016-11-30 16:15:11 +0100

    fix get logger problem

--- coap_testing_tool/test_coordinator/tests/tests.py
+++ coap_testing_tool/test_coordinator/tests/tests.py
@@ -1,8 +1,6 @@
 import unittest, logging, os
-from kombu import Connection
-from coap_testing_tool.test_coordinator.coordinator import Coordinator,import_teds
-from coap_testing_tool.utils.exceptions import CoordinatorError, TatError, SnifferError
-from coap_testing_tool import *
+from coap_testing_tool.test_coordinator.coordinator import *
+from coap_testing_tool import LOGDIR,TD_DIR
 
 TD_COAP = os.path.join(TD_DIR,'TD_COAP_CORE.yaml')
 
@@ -11,107 +9,105 @@ class CoordinatorTestCase(unittest.TestCase):
     def setUp(self):
 
         #print( str(os.environ['PYTHON_PATH']))
-        logging.basicConfig(level=logging.DEBUG)
-
-        conn = Connection(hostname=AMQP_SERVER,
-                          userid=AMQP_USER,
-                          password=AMQP_PASS,
-                          virtual_host=AMQP_VHOST,
-                          transport_options={'confirm_publish': True})
+        credentials = pika.PlainCredentials(AMQP_USER, AMQP_PASS)
+        connection = pika.BlockingConnection(pika.ConnectionParameters(
+            host=AMQP_SERVER,
+            virtual_host=AMQP_VHOST,
+            credentials = credentials))
 
         # it docs its a raw import
 
         # this tests import and the construction of Coordinator and test cases from yaml file
-        self.coord = Coordinator(conn,TD_COAP)
+        self.coord = Coordinator(connection,TD_COAP)
 
     def test_parse_yaml(self):
-        logging.debug("raw parse : ")
+        print("raw parse : ")
         it_docs = import_teds(TD_COAP)
         for d in self.coord.teds:
-            logging.debug(d)
-        logging.debug( it_docs)
+            print(d)
+        print( it_docs)
         for item in it_docs:
             print(str(type(item)))
-            logging.debug(str(type(item)))
-            logging.debug(str(item))
+            print(str(type(item)))
+            print(str(item))
 
-    def test_get_test_cases_as_list(self):
-        logging.debug("LIST OF TEST CASES: ")
-        ls = self.coord.get_test_cases_list()
+    def test_get_testcases_as_list(self):
+        print("LIST OF TEST CASES: ")
+        ls = self.coord.get_testcases_list()
         assert len(ls)==3
-        logging.debug(ls)
+        print(ls)
 
-    def test_select_test_case(self):
-        self.coord.select_test_case('TD_COAP_CORE_02_v01')
+    def test_select_testcase(self):
+        self.coord.select_testcase('TD_COAP_CORE_02_v01')
         assert self.coord.current_tc.id == 'TD_COAP_CORE_02_v01'
 
 
     def test_check_all_steps_finished(self):
         # this must not raise any errors during the iteration, control flow is done with None when iter is over!
         c = self.coord
-        tc = c.next_test_case()
+        tc = c.next_testcase()
         assert tc is not None
-        logging.debug("starting check_finished() method test")
+        print("starting check_finished() method test")
         for p in tc.sequence:
             assert tc.check_all_steps_finished() is False
             p.change_state('postponed')
-            logging.warning('step state: '+ str(p.state))
+            print('step state: '+ str(p.state))
         assert tc.check_all_steps_finished() is True
-        logging.debug("TD finished!")
+        print("TD finished!")
 
-    def test_stepping_over_the_test_cases(self):
+    def test_stepping_over_the_testcases(self):
         c = self.coord
         for i in range(4):
-            tc = c.next_test_case()
+            tc = c.next_testcase()
             if tc:
                 tc.change_state('skipped')
-            logging.info("iter over TCs: \n" + str(tc))
+            print("iter over TCs: \n" + str(tc))
         assert tc == None
 
 
     def test_stepping_over_the_steps_and_the_TCs(self):
         # this must not raise any errors during the iteration, control flow is done with None when iter is over!
         c = self.coord
-        tc = c.next_test_case()
+        tc = c.next_testcase()
         assert tc is not None
 
-        logging.debug("starting iteration over all steps in the TD")
-        logging.debug("starting with: " + tc.id)
+        print("starting iteration over all steps in the TD")
+        print("starting with: " + tc.id)
 
         while tc is not None:
-            logging.debug("running TC: "+ str(tc.id))
+            print("running TC: "+ str(tc.id))
             s = c.next_step()
 
             while s is not None:
-                logging.debug("\t passing: " + s.id)
+                print("\t passing: " + s.id)
                 s.change_state('postponed')
                 s = c.next_step()
 
             tc.change_state('skipped')
-            tc = c.next_test_case()
-        logging.debug("TD finished!")
+            tc = c.next_testcase()
+        print("TD finished!")
 
 
 
             # def test_step_execution(self):
     #     c = self.coord
-    #     tc = c.next_test_case()
-    #     logging.debug("starting execution of steps in the TD")
-    #     logging.debug("starting with: " + tc.id)
+    #     tc = c.next_testcase()
+    #     print("starting execution of steps in the TD")
+    #     print("starting with: " + tc.id)
     #     while tc is not None:
-    #         logging.debug("\t running TC: "+ str(tc.id))
+    #         print("\t running TC: "+ str(tc.id))
     #         res , s_next = c.execute_step()
-    #         logging.debug("\t return: %s next step %s" %(res , s_next))
+    #         print("\t return: %s next step %s" %(res , s_next))
     #         s = s_next
     #         while s is not None:
-    #             logging.debug("\t \t  step executed: %s , type: %s" %(s.id,s.type))
+    #             print("\t \t  step executed: %s , type: %s" %(s.id,s.type))
     #             res, s_next = c.execute_step()
-    #             logging.debug("\t \t  step executed RESULT: : %s " % (res))
+    #             print("\t \t  step executed RESULT: : %s " % (res))
     #             s = s_next
     #
     #
-    #         tc = c.next_test_case()
-    #     logging.debug("TD EXECUTION finished!")
+    #         tc = c.next_testcase()
+    #     print("TD EXECUTION finished!")
 
 
 if __name__ == '__main__':
