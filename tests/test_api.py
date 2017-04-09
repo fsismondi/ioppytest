1118ceb85fe6bd001651925fbced11f8ffe9eca8
Federico Sismondi <federicosismondi@gmail.com>
2017-04-09 18:31:17 +0200

    added some test for smoke testing the components api

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,6 +5,7 @@ from coap_testing_tool.utils.event_bus_messages import *
 from tests.database_pcap_base64 import *
 from urllib.parse import urlparse
 
+import unittest
 import pika
 import sys
 import time
@@ -15,8 +16,240 @@ import datetime
 
 COMPONENT_ID = 'fake_session'
 MESSAGES_WAIT_INTERVAL = 1 # in seconds
+AMQP_EXCHANGE = ''
+AMQP_URL = ''
 message_count = 0
-shutdown = False
+stop_generator_signal = False
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+logging.getLogger('pika').setLevel(logging.INFO)
+
+# for a typical user input, for a user (coap client) vs automated-iut ( coap server) session type:
+user_sequence = [
+    MsgTestSuiteGetStatus(),
+    MsgTestSuiteStart(),
+    MsgTestSuiteGetStatus(),
+    MsgTestCaseSkip(testcase_id='TD_COAP_CORE_02_v01'),
+    MsgTestSuiteGetStatus(),
+    MsgTestCaseSkip(testcase_id='TD_COAP_CORE_03_v01'),
+    MsgTestSuiteGetStatus(),
+    MsgTestCaseSkip(testcase_id='TD_COAP_CORE_04_v01'),
+    MsgTestSuiteGetStatus(),
+    MsgTestCaseStart(),
+    MsgTestSuiteGetStatus(),
+    MsgStimuliExecuted(),
+    MsgTestSuiteGetStatus(),
+    MsgVerifyResponse(),
+    MsgTestSuiteGetStatus(),
+    MsgVerifyResponse(
+            verify_response=False,
+            description='User indicates that IUT didnt behave as expected '),
+    MsgTestSuiteGetStatus(),
+    # at this point we should see a TC verdict
+    MsgTestCaseRestart(),
+    MsgTestSuiteGetStatus(),
+    MsgTestSuiteAbort(),
+    MsgTestSuiteGetStatus(),
+]
+
+forced_errors_service_api_calls=[
+    MsgInteropTestCaseAnalyze(),
+    MsgInteropTestCaseAnalyze(
+            testcase_id="TD_COAP_CORE_01",
+            testcase_ref="http://f-interop.paris.inria.fr/tests/TD_COAP_CORE_01_v01",
+            file_enc="pcap_base64",
+            filename="TD_COAP_CORE_01.pcap",
+            value=PCAP_empty_base64,
+    ),
+]
+
+service_api_calls = [
+    # TAT calls
+    MsgTestSuiteGetStatus(),
+    MsgTestSuiteGetTestCases(),
+    MsgInteropTestCaseAnalyze(
+            testcase_id="TD_COAP_CORE_01",
+            testcase_ref="http://f-interop.paris.inria.fr/tests/TD_COAP_CORE_01_v01",
+            file_enc="pcap_base64",
+            filename="TD_COAP_CORE_01.pcap",
+            value=PCAP_TC_COAP_01_base64,
+    ),
+
+    # Sniffer calls (order matters!)
+    MsgSniffingStart(
+            capture_id='TD_COAP_CORE_01',
+            filter_if='tun0',
+            filter_proto='udp port 5683'
+    ),
+    MsgSniffingStop(),
+    MsgSniffingGetCapture(tescase_id='TD_COAP_CORE_01'),
+    MsgSniffingGetCaptureLast(),
+
+    # Dissector calls
+    MsgDissectionDissectCapture(),
+    MsgDissectionDissectCapture(
+            file_enc="pcap_base64",
+            filename="TD_COAP_CORE_01.pcap",
+            protocol_selection='coap',
+            value=PCAP_TC_COAP_01_base64,
+    ),
+    # complete dissection of pcap
+    MsgDissectionDissectCapture(
+            file_enc="pcap_base64",
+            filename="TD_COAP_CORE_01.pcap",
+            value=PCAP_TC_COAP_01_base64,
+    ),
+    # complete dissection of pcap with extra TCP traffic
+    MsgDissectionDissectCapture(
+            file_enc="pcap_base64",
+            filename="TD_COAP_CORE_01.pcap",
+            value=PCAP_TC_COAP_01_mingled_with_tcp_traffic_base64,
+    ),
+    # same as dis4 but filtering coap messages
+    MsgDissectionDissectCapture(
+            file_enc="pcap_base64",
+            filename="TD_COAP_CORE_01.pcap",
+            protocol_selection='coap',
+            value=PCAP_TC_COAP_01_mingled_with_tcp_traffic_base64,
+    ),
+    # pcap sniffed using AMQP based packet sniffer
+    MsgDissectionDissectCapture(
+            file_enc="pcap_base64",
+            filename="TD_COAP_CORE_01.pcap",
+            value=PCAP_COAP_GET_OVER_TUN_INTERFACE_base64,
+    )
+]
+
+class ApiTests(unittest.TestCase):
+
+    def setUp(self):
+
+        global stop_generator_signal
+        stop_generator_signal = False
+
+        import_env_vars()
+
+        self.conn = pika.BlockingConnection(pika.URLParameters(AMQP_URL))
+        self.channel = self.conn.channel()
+
+        services_queue_name = 'services_queue@%s' % COMPONENT_ID
+
+        # lets' first clean up the queue
+        self.channel.queue_delete(queue=services_queue_name)
+
+        self.channel.queue_declare(queue=services_queue_name)
+        self.channel.queue_bind(exchange=AMQP_EXCHANGE, queue=services_queue_name, routing_key='#')
+        self.channel.basic_qos(prefetch_count=1)
+        self.channel.basic_consume(validate_message, queue=services_queue_name)
+
+    def tearDown(self):
+        self.conn.close()
+
+    def test_user_emulation(self):
+
+        messages = [] # list of messages to send
+        messages += user_sequence
+        messages.append(MsgSessionTerminate())  # message that triggers stop_generator_signal
+
+        thread_msg_gen = MessageGenerator(AMQP_URL, AMQP_EXCHANGE, messages)
+        logger.debug("Starting Message Generator thread ")
+        thread_msg_gen.start()
+
+        try:
+            self.channel.start_consuming()
+        except NonCompliantMessageFormatError as e:
+            thread_msg_gen.stop()
+            assert False, str(e)
+
+
+
+    def test_testing_tool_internal_services(self):
+        messages = [] # list of messages to send
+        messages += service_api_calls
+        messages.append(MsgSessionTerminate())  # message that triggers stop_generator_signal
+        services_queue_name = 'bus_errors_queue@%s' % COMPONENT_ID
+
+        channel = self.conn.channel()
+        # lets' first clean up the queue
+        channel.queue_delete(queue=services_queue_name)
+
+        channel.queue_declare(queue=services_queue_name)
+        channel.queue_bind(exchange=AMQP_EXCHANGE,
+                           queue=services_queue_name,
+                           routing_key='*.error.*')
+        # for getting the terminate signal
+        channel.queue_bind(exchange=AMQP_EXCHANGE,
+                           queue=services_queue_name,
+                           routing_key=MsgSessionTerminate.routing_key)
+        channel.basic_qos(prefetch_count=1)
+        channel.basic_consume( check_for_bus_error, queue=services_queue_name)
+
+        thread_msg_gen = MessageGenerator(AMQP_URL, AMQP_EXCHANGE, messages)
+        logger.debug("Starting Message Generator thread ")
+        thread_msg_gen.start()
+
+        try:
+            channel.start_consuming()
+        except Exception as e:
+            thread_msg_gen.stop()
+            assert False, str(e)
+
+
+
+    def test_non_existent_types_in_message_library_dont_generate_validation_errors(self):
+        messages = [] # list of messages to send
+        m = MsgInteropTestCaseAnalyze()
+        m._type = 'some.non.existent.message.type'
+        messages += [m]
+        messages += service_api_calls
+        messages.append(MsgSessionTerminate())  # message that triggers stop_generator_signal
+
+        thread_msg_gen = MessageGenerator(AMQP_URL, AMQP_EXCHANGE, messages)
+        logger.debug("Starting Message Generator thread ")
+        thread_msg_gen.start()
+
+        try:
+            self.channel.start_consuming()
+        except NonCompliantMessageFormatError as e:
+            thread_msg_gen.stop()
+            assert False, str(e)
+
+
+# # # # # # AUXILIARY METHODS # # # # # # #
+
+
+def import_env_vars():
+    global AMQP_EXCHANGE
+    global AMQP_URL
+
+    try:
+        AMQP_EXCHANGE = str(os.environ['AMQP_EXCHANGE'])
+    except KeyError as e:
+        AMQP_EXCHANGE = "default"
+
+    try:
+        AMQP_URL = str(os.environ['AMQP_URL'])
+        p = urlparse(AMQP_URL)
+        AMQP_USER = p.username
+        AMQP_SERVER = p.hostname
+        logger.info(
+                "Env variables imported for AMQP connection, User: {0} @ Server: {1} ".format(AMQP_USER, AMQP_SERVER)
+        )
+    except KeyError as e:
+        logger.error('Cannot retrieve environment variables for AMQP connection. Loading defaults..')
+        # load default values
+        AMQP_URL = "amqp://{0}:{1}@{2}/{3}".format("guest", "guest", "localhost", "/")
+
+    connection = pika.BlockingConnection(pika.URLParameters(AMQP_URL))
+
+    # in case its not declared
+    connection.channel().exchange_declare(exchange=AMQP_EXCHANGE,
+                                          type='topic',
+                                          durable=True,
+                                          )
+    connection.close()
 
 
 def publish_message(channel, message):
@@ -29,19 +262,44 @@ def publish_message(channel, message):
             body=message.to_json(),
     )
 
-def _shutdown():
-    global shutdown
-    logging.info("The test is finished!")
-    shutdown = True
+def stop_generator():
+    global stop_generator_signal
+    logger.debug("The test is finished!")
+    stop_generator_signal = True
 
 
-def on_request(ch, method, props, body):
+def check_for_bus_error(ch, method, props, body):
+
+    try:
+        m = Message.from_json(body)
+        if isinstance(m,MsgSessionTerminate):
+            ch.stop_consuming()
+            return
+    except:
+        pass
+
+    list_of_audited_components = [
+        'tat',
+        'test_coordinator',
+        'packer_router',
+        'sniffer',
+        'dissector',
+
+    ]
+    r_key = method.routing_key
+    logger.info('Auditing: %s'%r_key)
+
+    for c in list_of_audited_components:
+        if c in r_key:
+            assert False, 'audited component %s pushed an error into the bus' %c
+
+
+def validate_message(ch, method, props, body):
         global message_count
         # obj hook so json.loads respects the order of the fields sent -just for visualization purposeses-
         req_body_dict = json.loads(body.decode('utf-8'),object_pairs_hook=OrderedDict)
         ch.basic_ack(delivery_tag=method.delivery_tag)
         message_count += 1
-
 
         print('\n* * * * * * MESSAGE SNIFFED by INSPECTOR (%s) * * * * * * *'%message_count)
         print("TIME: %s"%datetime.datetime.time(datetime.datetime.now()))
@@ -62,17 +320,16 @@ def on_request(ch, method, props, body):
             print('* * * * * * * * * * * * * * * * * * * * * * * * *  \n')
             raise Exception
 
-
-        # lets check messages agains the messaging library
+        # lets check messages against the messaging library
         list_of_messages_to_check = list(message_types_dict.keys())
         if req_body_dict['_type'] in list_of_messages_to_check:
             m = Message.from_json(body)
             try:
-                if isinstance(m,MsgTestSuiteAbort):
+                if isinstance(m,MsgSessionTerminate):
                     ch.stop_consuming()
-                    _shutdown()
+                    stop_generator()
                 else:
-                    print(repr(m))
+                    logger.debug(repr(m))
             except NonCompliantMessageFormatError as e:
                 print('* * * * * * API VALIDATION WARNING * * * * * * * ')
                 print("AMQP MESSAGE LIBRARY COULD PROCESS JSON MESSAGE")
@@ -82,31 +339,29 @@ def on_request(ch, method, props, body):
 
 class MessageGenerator(threading.Thread):
 
-    def __init__(self, amqp_url, amqp_exchange):
+    def __init__(self, amqp_url, amqp_exchange, messages_list):
         threading.Thread.__init__(self)
+        self.messages = messages_list
         self.connection = pika.BlockingConnection(pika.URLParameters(amqp_url))
-        self.channel = connection.channel()
-        logging.info("AMQP connection established")
+        self.channel = self.connection.channel()
+        logger.info("AMQP connection established")
 
     def run(self):
         global MESSAGES_WAIT_INTERVAL
-        global shutdown
+        global stop_generator_signal
 
-        logging.info(
+        logger.info(
                 "let's start 'blindly' generating the messages which take part on a coap session (for a coap client)"
         )
 
-        messages = [] # list of messages to send
-        messages.append(MsgTestSuiteAbort())  # message that triggers shutdown
-        messages += user_sequence + service_api_calls
-
         try:
-            while not shutdown:
+            while not stop_generator_signal:
                 time.sleep(MESSAGES_WAIT_INTERVAL)
-                m = messages.pop()
-                publish_message(channel, m)
-                logging.info("Publishing in the bus: %s" % repr(m))
+                m = self.messages.pop(0)
+                publish_message(self.channel, m)
+                logger.info("Publishing in the bus: %s" % repr(m))
         except IndexError:
+            # list finished, lets wait so all messages are sent and processed
             time.sleep(5)
             pass
 
@@ -114,150 +369,6 @@ class MessageGenerator(threading.Thread):
         self.connection.close()
 
 
-if __name__ == '__main__':
 
-    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
-    logging.getLogger('pika').setLevel(logging.INFO)
-
-    try:
-        AMQP_EXCHANGE = str(os.environ['AMQP_EXCHANGE'])
-    except KeyError as e:
-        AMQP_EXCHANGE = "default"
-
-    try:
-        AMQP_URL = str(os.environ['AMQP_URL'])
-        p = urlparse(AMQP_URL)
-        AMQP_USER = p.username
-        AMQP_SERVER = p.hostname
-        logging.info(
-                "Env variables imported for AMQP connection, User: {0} @ Server: {1} ".format(AMQP_USER, AMQP_SERVER)
-        )
-    except KeyError as e:
-        print('Cannot retrieve environment variables for AMQP connection. Loading defaults..')
-        # load default values
-        AMQP_URL = "amqp://{0}:{1}@{2}/{3}".format("guest", "guest", "localhost", "/")
-
-    connection = pika.BlockingConnection(pika.URLParameters(AMQP_URL))
-    channel = connection.channel()
-    logging.info("AMQP connection established")
-
-
-
-    # in case its not declared
-    connection.channel().exchange_declare(exchange=AMQP_EXCHANGE,
-                                          type='topic',
-                                          durable=True,
-                                          )
-
-
-    # for a typical user input, for a user (coap client) vs automated-iut ( coap server) session type:
-    user_sequence = [
-        MsgTestSuiteStart(),
-        MsgTestCaseSkip(testcase_id='TD_COAP_CORE_02_v01'),
-        MsgTestCaseSkip(testcase_id='TD_COAP_CORE_03_v01'),
-        MsgTestCaseSkip(testcase_id='TD_COAP_CORE_04_v01'),
-        MsgTestCaseStart(),
-        MsgStimuliExecuted(),
-        MsgVerifyResponse(),
-        MsgVerifyResponse(
-                verify_response=False,
-                description='User indicates that IUT didnt behave as expected '),
-        # at this point we should see a TC verdict
-        MsgTestCaseRestart(),
-    ]
-
-    service_api_calls = [
-
-        # TAT calls
-        MsgTestSuiteGetStatus(),
-        MsgTestSuiteGetTestCases(),
-        MsgInteropTestCaseAnalyze(),
-        MsgInteropTestCaseAnalyze(
-                testcase_id="TD_COAP_CORE_01",
-                testcase_ref="http://f-interop.paris.inria.fr/tests/TD_COAP_CORE_01_v01",
-                file_enc="pcap_base64",
-                filename="TD_COAP_CORE_01.pcap",
-                value=PCAP_empty_base64,
-        ),
-        MsgInteropTestCaseAnalyze(
-                testcase_id="TD_COAP_CORE_01",
-                testcase_ref="http://f-interop.paris.inria.fr/tests/TD_COAP_CORE_01_v01",
-                file_enc="pcap_base64",
-                filename="TD_COAP_CORE_01.pcap",
-                value=PCAP_TC_COAP_01_base64,
-        ),
-
-        # Sniffer calls
-        MsgSniffingStart(
-                capture_id='TD_COAP_CORE_01',
-                filter_if='tun0',
-                filter_proto='udp port 5683'
-        ),
-        MsgSniffingStop(),
-        MsgSniffingGetCapture(tescase_id='TD_COAP_CORE_01'),
-        MsgSniffingGetCaptureLast(),
-
-        # Dissector calls
-        MsgDissectionDissectCapture(),
-        MsgDissectionDissectCapture(
-                file_enc="pcap_base64",
-                filename="TD_COAP_CORE_01.pcap",
-                protocol_selection='coap',
-                value=PCAP_TC_COAP_01_base64,
-        ),
-        # complete dissection of pcap
-        MsgDissectionDissectCapture(
-                file_enc="pcap_base64",
-                filename="TD_COAP_CORE_01.pcap",
-                value=PCAP_TC_COAP_01_base64,
-        ),
-        # complete dissection of pcap with extra TCP traffic
-        MsgDissectionDissectCapture(
-                file_enc="pcap_base64",
-                filename="TD_COAP_CORE_01.pcap",
-                value=PCAP_TC_COAP_01_mingled_with_tcp_traffic_base64,
-        ),
-        # same as dis4 but filtering coap messages
-        MsgDissectionDissectCapture(
-                file_enc="pcap_base64",
-                filename="TD_COAP_CORE_01.pcap",
-                protocol_selection='coap',
-                value=PCAP_TC_COAP_01_mingled_with_tcp_traffic_base64,
-        ),
-        # pcap sniffed using AMQP based packet sniffer
-        MsgDissectionDissectCapture(
-                file_enc="pcap_base64",
-                filename="TD_COAP_CORE_01.pcap",
-                value=PCAP_COAP_GET_OVER_TUN_INTERFACE_base64,
-        )
-    ]
-
-
-    thread_msg_gen = MessageGenerator( AMQP_URL, AMQP_EXCHANGE)
-    #thread_inspector.daemon = True
-    print("Starting Message Generator thread ")
-    thread_msg_gen.start()
-
-    # queues & default exchange declaration
-    message_count = 0
-
-    services_queue_name = 'services_queue@%s' % COMPONENT_ID
-
-    channel.queue_delete(queue=services_queue_name)
-
-    channel.queue_declare(queue=services_queue_name)
-
-    channel.queue_bind(exchange=AMQP_EXCHANGE,
-                            queue=services_queue_name,
-                            routing_key='#')
-
-    channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(on_request, queue=services_queue_name)
-
-    try:
-        channel.start_consuming()
-    except Exception as e:
-        thread_msg_gen.stop()
-        sys.exit(1)
 
 
