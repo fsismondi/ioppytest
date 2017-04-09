5116d0727042334f38f8a76829fe742a953a9d68
Federico Sismondi <federicosismondi@gmail.com>
2017-04-09 17:36:12 +0200

    added amqp_request (for request/reply calls) and publish message using Message class

--- coap_testing_tool/utils/amqp_synch_call.py
+++ coap_testing_tool/utils/amqp_synch_call.py
@@ -10,9 +10,22 @@ from threading import Timer
 from coap_testing_tool.utils.exceptions import TatError, SnifferError,CoordinatorError, AmqpMessageError
 from coap_testing_tool import AMQP_URL, AMQP_EXCHANGE
 from collections import OrderedDict
+from coap_testing_tool.utils.event_bus_messages import *
+
+# timeout in seconds
+AMQP_REPLY_TOUT = 10
 
 
 def amqp_reply(channel, props, response):
+    """
+    DEPRICATED! Just generate a reply message using MsgReply class
+    and use publish_message method
+
+    :param channel:
+    :param props:
+    :param response:
+    :return:
+    """
     # check first that sender didnt forget about reply to and corr id
     try:
         reply_to = props.reply_to
@@ -34,8 +47,86 @@ def amqp_reply(channel, props, response):
         )
     )
 
-# timeout in seconds
-AMQP_REPLY_TOUT = 10
+def publish_message(channel, message):
+    """ Published which uses message object metadata
+
+    :param channel:
+    :param message:
+    :return:
+    """
+
+    properties = pika.BasicProperties(**message.get_properties())
+
+    channel.basic_publish(
+            exchange=AMQP_EXCHANGE,
+            routing_key=message.routing_key,
+            properties=properties,
+            body=message.to_json(),
+    )
+
+def amqp_request(request_message : Message, component_id : str):
+    # check first that sender didnt forget about reply to and corr id
+    assert(request_message.reply_to)
+    assert (request_message.correlation_id)
+
+    # setup blocking connection, do not reuse the conection from coord, it needs to be a new one
+    connection = pika.BlockingConnection(pika.URLParameters(AMQP_URL))
+    response = None
+
+    channel = connection.channel()
+    reply_queue_name = 'amqp_rpc_%s@%s' %(str(uuid.uuid4())[:8],component_id)
+
+    result = channel.queue_declare(queue=reply_queue_name)
+
+    callback_queue = result.method.queue
+
+    # by convention routing key of answer is routing_key + .reply
+    channel.queue_bind(
+            exchange=AMQP_EXCHANGE,
+            queue=callback_queue,
+            routing_key=request_message.reply_to
+    )
+
+    channel.basic_publish(
+            exchange=AMQP_EXCHANGE,
+            routing_key=request_message.routing_key,
+            properties=pika.BasicProperties(**request_message.get_properties()),
+            body=request_message.to_json(),
+    )
+
+    time.sleep(0.2)
+    max_retries = 5
+
+    method, props, body = channel.basic_get(reply_queue_name)
+
+    while max_retries > 0:
+        if hasattr(props, 'correlation_id') and props.correlation_id == request_message.correlation_id:
+            break
+        method, props, body = channel.basic_get(reply_queue_name)
+        max_retries -= 1
+        time.sleep(0.5)
+
+    if max_retries > 0 :
+        body_dict = json.loads(body.decode('utf-8'),object_pairs_hook=OrderedDict)
+        response = MsgReply(request_message, **body_dict)
+
+    else:
+        raise AmqpMessageError("Response timeout! rkey: %s , request type: %s"
+                               %(
+                                    request_message.routing_key,
+                                    request_message._type
+                               )
+                               )
+
+    # cleaning up
+    channel.queue_delete(reply_queue_name)
+    connection.close()
+
+    return response
+
+
+
+
 
 class AmqpSynchronousCallClient:
 
@@ -102,18 +193,23 @@ class AmqpSynchronousCallClient:
 
         return json.loads(self.response.decode('utf-8'),object_pairs_hook=OrderedDict)
 
-#this is just an example of usage where we ask the sniffer for a pcap capture and we save it disk after:
 if __name__ == '__main__':
-    connection = pika.BlockingConnection(pika.URLParameters(AMQP_URL))
-    amqpRPCClient = AmqpSynchronousCallClient("dummy_component")
 
-    body = {'_type': 'sniffing.getcapture', 'testcase_id': 'testcase pepito'}
-    ret = amqpRPCClient.call("control.sniffing.service", body=body)
+    #this is just an example of usage where we ask the sniffer for a pcap capture and we save it disk after:
+    # connection = pika.BlockingConnection(pika.URLParameters(AMQP_URL))
+    # amqpRPCClient = AmqpSynchronousCallClient("dummy_component")
+    #
+    # body = {'_type': 'sniffing.getcapture', 'testcase_id': 'testcase pepito'}
+    # ret = amqpRPCClient.call("control.sniffing.service", body=body)
+    #
+    # out = ret['value']
+    # filename = ret['filename']
+    #
+    # # save to file
+    # with open("./"+filename, "wb") as file2:
+    #     file2.write(base64.b64decode(out))
 
-    out = ret['value']
-    filename = ret['filename']
-
-    # save to file
-    with open("./"+filename, "wb") as file2:
-        file2.write(base64.b64decode(out))
+    m = MsgSniffingGetCapture()
+    r = amqp_request(m, 'someImaginaryComponent')
+    print(repr(r))
 
