6311a095e097d7215139c61235a49740d3d1b732
Federico Sismondi <federicosismondi@gmail.com>
2017-04-11 09:02:02 +0200

    now sniffer uses messaging lib

--- coap_testing_tool/sniffer/packet_sniffer.py
+++ coap_testing_tool/sniffer/packet_sniffer.py
@@ -8,19 +8,13 @@ import sys
 import base64
 import traceback
 import pika
-import logging
-import json
-from collections import OrderedDict
-from coap_testing_tool.utils.amqp_synch_call import amqp_reply, publish_message
+from coap_testing_tool.utils.amqp_synch_call import publish_message
 from coap_testing_tool import TMPDIR, DATADIR, LOGDIR, AMQP_EXCHANGE, AMQP_URL
 from coap_testing_tool.utils.rmq_handler import RabbitMQHandler, JsonFormatter
 from coap_testing_tool.utils.event_bus_messages import *
 
 COMPONENT_ID = 'packet_sniffer'
-
-ALLOWED_EXTENSIONS = set(['pcap'])
 last_capture = None
-
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
@@ -40,6 +34,7 @@ rabbitmq_handler.setFormatter(json_formatter)
 logger.addHandler(rabbitmq_handler)
 logger.setLevel(logging.DEBUG)
 
+
 def on_request(ch, method, props, body):
     # ack message received
     ch.basic_ack(delivery_tag=method.delivery_tag)
@@ -47,185 +42,165 @@ def on_request(ch, method, props, body):
     global last_capture
 
     try:
+        props_dict = {
+            'content_type': props.content_type,
+            'delivery_mode': props.delivery_mode,
+            'correlation_id': props.correlation_id,
+            'reply_to': props.reply_to,
+            'message_id': props.message_id,
+            'timestamp': props.timestamp,
+            'user_id': props.user_id,
+            'app_id': props.app_id,
+        }
         request = Message.from_json(body)
+        request.update_properties(**props_dict)
+
     except Exception as e:
         logger.error(str(e))
         return
 
-    req_dict = json.loads(body.decode('utf-8'))
 
-    # horribly long composition of methods,but  needed for keeping the order of fields of the received json object
-    logger.debug('[event queue callback] service request received on the queue: %s || %s'
-                 % (method.routing_key, json.dumps(json.loads(body.decode('utf-8'),object_pairs_hook=OrderedDict))))
-
-    try:
-        req_type = req_dict['_type']
-    except Exception as e:
-        logger.error('No _type found on event meesage : %s'%str(req_dict))
-
-    if method.routing_key in ('control.sniffing.info','control.sniffing.error','control.sniffing.service.reply'):
-        # ignore echo message
-        logger.debug('Ignoring echo message: %s with r_key:%s' %(str(req_dict),method.routing_key))
-
-
-    elif req_type == 'sniffing.getlastcapture':
-        logger.info('Processing %s request'%req_type)
+    if isinstance(request, MsgSniffingGetCaptureLast):
+        logger.info('Processing request: %s' % repr(request))
 
         if last_capture:
             capture_id = last_capture
 
             try:
-                file = TMPDIR +'/%s.pcap'%capture_id
+                file = TMPDIR + '/%s.pcap' % capture_id
                 # check if the size of PCAP is not zero
-                if os.path.getsize(file)== 0:
-                    #raise SnifferError(message='Problem encountered with the requested PCAP')
+                if os.path.getsize(file) == 0:
+                    # raise SnifferError(message='Problem encountered with the requested PCAP')
                     logger.error('Problem encountered with the requested PCAP')
                     return
 
             except FileNotFoundError as fne:
-                logger.error('Coulnt retrieve file %s from dir'%file)
+                publish_message(
+                        ch,
+                        MsgErrorReply(request, error_message=str(fne))
+                )
+                logger.error(str(fne))
                 return
-                #raise
 
             logger.info("Encoding PCAP file into base64 ...")
 
-            # do not dump into PCAP_DIR, coordinator puts the PCAPS
-            with open(TMPDIR+"/%s.pcap"%capture_id, "rb") as file:
+            # do not dump into PCAP_DIR, coordinator puts the PCAPS there
+            with open(TMPDIR + "/%s.pcap" % capture_id, "rb") as file:
                 enc = base64.b64encode(file.read())
 
-            # lets build response
-            response = OrderedDict()
-            response.update({'_type': req_type})
-            response.update({'ok': True})
-            response.update({'file_enc':'pcap_base64'})
-            response.update({'filename':'%s.pcap'%capture_id})
-            response.update({'value': enc.decode("utf-8")})
+            response = MsgSniffingGetCaptureLastReply(
+                    request,
+                    ok=True,
+                    filename='%s.pcap' % capture_id,
+                    value=enc.decode("utf-8")
 
-            logger.info("Response ready, PCAP bytes: \n" + str(response))
+            )
+
+            logger.info("Response ready, PCAP bytes: \n" + repr(response))
             logger.info("Sending response through AMQP interface ...")
-
-            amqp_reply(ch, props, response)
+            publish_message(ch, response)
 
         else:
             err_mess = 'No previous capture found.'
-            publish_message(
-                    ch,
-                    MsgErrorReply(
-                        request,
-                        error_message=err_mess
-                    )
-            )
+            m_resp = MsgErrorReply(request, error_message=err_mess)
+            publish_message(ch, m_resp)
             logger.error(err_mess)
-
             return
 
+    elif isinstance(request, MsgSniffingGetCapture):
 
-    elif req_type == 'sniffing.getcapture':
-        logger.info('Processing %s request'%req_type)
-        try:
-            capture_id = req_dict['capture_id']
-
-        except Exception as e:
-            err_mess = 'Failed to get capture_id field from request'
-            publish_message(
-                    ch,
-                    MsgErrorReply(
-                        request,
-                        error_message=str(err_mess)
-                    )
-            )
-            logger.error(str(e))
-
-            return
+        logger.info('Processing request: %s' % repr(request))
 
         try:
-            file = TMPDIR +'/%s.pcap'%capture_id
+            capture_id = request.capture_id
+            file = TMPDIR + '/%s.pcap' % capture_id
+
             # check if the size of PCAP is not zero
-            if os.path.getsize(file)== 0:
-                #raise SnifferError(message='Problem encountered with the requested PCAP')
+            if os.path.getsize(file) == 0:
+                # raise SnifferError(message='Problem encountered with the requested PCAP')
                 logger.error('Problem encountered with the requested PCAP')
                 return
+
         except FileNotFoundError as fne:
-            logger.error('Coulnt retrieve file %s from dir'%file)
+            logger.error('Coulnt retrieve file %s from dir' % file)
+            logger.error(str(fne))
+            publish_message(
+                    ch,
+                    MsgErrorReply(
+                            request,
+                            error_message=str(fne)
+                    )
+            )
             return
-            #raise
 
         logger.info("Encoding PCAP file into base64 ...")
 
-        # do not dump into PCAP_DIR, coordinator puts the PCAPS
-        with open(TMPDIR+"/%s.pcap"%capture_id, "rb") as file:
+        # do not dump into PCAP_DIR, coordinator puts the PCAPS there
+        with open(TMPDIR + "/%s.pcap" % capture_id, "rb") as file:
             enc = base64.b64encode(file.read())
 
-        # lets build response
-        response = OrderedDict()
-        response.update({'_type': req_type})
-        response.update({'ok': True})
-        response.update({'file_enc':'pcap_base64'})
-        response.update({'filename':'%s.pcap'%capture_id})
-        response.update({'value': enc.decode("utf-8")})
+        response = MsgSniffingGetCaptureReply(
+                request,
+                ok=True,
+                filename='%s.pcap' % capture_id,
+                value=enc.decode("utf-8")
 
-        logger.info("Response ready, PCAP bytes: \n" + str(response))
+        )
+
+        logger.info("Response ready, PCAP bytes: \n" + repr(response))
         logger.info("Sending response through AMQP interface ...")
+        publish_message(ch, response)
+        return
 
-        amqp_reply(ch, props, response)
-
-    elif req_type == 'sniffing.start':
-        logger.info('Processing %s request' % req_type)
+    elif isinstance(request, MsgSniffingStart):
+        logger.info('Processing request: %s' % repr(request))
         try:
-            capture_id = req_dict['capture_id']
+            capture_id = request.capture_id
         except:
-            #raise ApiMessageFormatError(message='No capture_id provided')
-            logger.error('No capture id provided')
+            err_mess = 'No capture id provided'
+            m_resp = MsgErrorReply(request, error_message=err_mess)
+            publish_message(ch, m_resp)
+            logger.error(err_mess)
             return
 
         filename = TMPDIR + '/' + capture_id + ".pcap"
         filter_if = ''
 
         try:
-            filter_if = req_dict['filter_if']
+            filter_if = request.filter_if
         except:
             logger.warning('No interface (filter_if) name provided')
 
         try:
-            filter_proto = req_dict['filter_proto']
+            filter_proto = request.filter_proto
         except:
             logger.warning('No filter_proto provided')
 
-
-        # TODO delete if there's already a file with the capture_id
-
         try:
-            _launch_sniffer(filename,filter_if,filter_proto)
+            _launch_sniffer(filename, filter_if, filter_proto)
         except:
-            #raise SnifferError('Didnt succeed starting the capture')
             logger.error('Didnt succeed starting the capture')
 
         # lets keep track of the undergoing capture name
         last_capture = capture_id
 
-        # lets build response
-        response = OrderedDict()
-        response.update({'_type': req_type})
-        response.update({'ok': True})
-        amqp_reply(ch, props, response)
+        response = MsgReply(request)  # by default sends ok = True
+        publish_message(ch, response)
 
-    elif req_type == 'sniffing.stop':
+    elif isinstance(request, MsgSniffingStop):
 
-        logger.info('Processing %s request' % req_type)
+        logger.info('Processing request: %s' % repr(request))
 
         try:
             _stop_sniffer()
         except:
             logger.error('Didnt succeed stopping the sniffer')
 
-        # lets build response
-        response = OrderedDict()
-        response.update({'_type': req_type})
-        response.update({'ok': True})
-        amqp_reply(ch, props, response)
+        response = MsgReply(request)  # by default sends ok = True
+        publish_message(ch, response)
 
     else:
-        logger.error('Wrong request received: %s' % str(req_dict))
+        logger.debug('Ignoring unrecognised service request: %s' % repr(request))
 
 
 ### IMPLEMENTATION OF SERVICES ###
@@ -234,9 +209,9 @@ def _launch_sniffer(filename, filter_if, filter_proto):
     logger.info('Launching packet capture..')
 
     if filter_proto is None:
-        filter_proto=''
+        filter_proto = ''
 
-    if (filter_if is None ) or (filter_if==''):
+    if (filter_if is None) or (filter_if == ''):
         sys_type = platform.system()
         if sys_type == 'Darwin':
             filter_if = 'lo0'
@@ -253,10 +228,11 @@ def _launch_sniffer(filename, filter_if, filter_proto):
 
     params = 'tcpdump -K -i ' + filter_if + ' -s 200 ' + ' -U -w ' + filename + ' ' + '&'
     os.system(params)
-    logger.info('creating process tcpdump with: %s'%params)
+    logger.info('creating process tcpdump with: %s' % params)
     # TODO we need to catch tcpdump: <<tun0: No such device exists>> from stderr
 
     return True
+
 
 def _stop_sniffer():
     proc = subprocess.Popen(["pkill", "-INT", "tcpdump"], stdout=subprocess.PIPE)
@@ -299,17 +275,16 @@ if __name__ == '__main__':
         logger.error(' AMQP cannot be established, is message broker up? \n More: %s' % traceback.format_exc())
         sys.exit(1)
 
-
     channel.basic_qos(prefetch_count=1)
     channel.basic_consume(on_request, queue='services_queue@%s' % COMPONENT_ID)
 
     channel.basic_publish(
-        body=json.dumps({'message': '%s is up!'%COMPONENT_ID,"_type": 'sniffing.ready'}),
-        exchange=AMQP_EXCHANGE,
-        routing_key='control.session.bootstrap',
-        properties=pika.BasicProperties(
-            content_type='application/json',
-        )
+            body=json.dumps({'message': '%s is up!' % COMPONENT_ID, "_type": 'sniffing.ready'}),
+            exchange=AMQP_EXCHANGE,
+            routing_key='control.session.bootstrap',
+            properties=pika.BasicProperties(
+                    content_type='application/json',
+            )
     )
 
     try:
@@ -324,13 +299,6 @@ if __name__ == '__main__':
         logger.error(' Unexpected error \n More: %s' % traceback.format_exc())
         sys.exit(1)
     finally:
-        #close AMQP connection
+        # close AMQP connection
         if connection:
             connection.close()
-        sys.exit(1)
-
-
-
-
-
-
