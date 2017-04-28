cbff2c0e2adca8a2c9764654726661f9044f809b
Federico Sismondi <federicosismondi@gmail.com>
2017-04-28 15:59:08 +0200

    extend messaging library usage

--- coap_testing_tool/sniffer/packet_sniffer.py
+++ coap_testing_tool/sniffer/packet_sniffer.py
@@ -278,14 +278,11 @@ if __name__ == '__main__':
     channel.basic_qos(prefetch_count=1)
     channel.basic_consume(on_request, queue='services_queue@%s' % COMPONENT_ID)
 
-    channel.basic_publish(
-            body=json.dumps({'message': '%s is up!' % COMPONENT_ID, "_type": 'sniffing.ready'}),
-            exchange=AMQP_EXCHANGE,
-            routing_key='control.session.bootstrap',
-            properties=pika.BasicProperties(
-                    content_type='application/json',
-            )
+    msg = MsgTestingToolComponentReady(
+        component = 'sniffing'
     )
+    publish_message(channel, msg )
+
 
     try:
         logger.info("Awaiting AMQP requests on topic: control.sniffing.service")
