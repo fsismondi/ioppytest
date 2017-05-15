47187d205b1b1ed9dc4325885ca97ee265342b07
Federico Sismondi <federicosismondi@gmail.com>
2017-05-15 17:47:31 +0200

    refact

--- coap_testing_tool/automated_implementations/coap_client_coapthon/automated_iut.py
+++ coap_testing_tool/automated_implementations/coap_client_coapthon/automated_iut.py
@@ -9,9 +9,11 @@ import datetime
 import os
 import signal
 from coap_testing_tool.utils.event_bus_messages import *
+from coap_testing_tool.utils.amqp_synch_call import publish_message
 from coap_testing_tool import AMQP_URL, AMQP_EXCHANGE
 
-COMPONENT_ID = 'finterop_coap_client_test_executor'
+
+COMPONENT_ID = 'automated_iut'
 
 # timeout in seconds
 STIMULI_HANDLER_TOUT = 10
@@ -23,12 +25,12 @@ IUT_CMD = [
 
 
 # mapping message's stimuli id -> CoAPthon (coap client) commands
-coap_client_tc_stimuli_commands = {
+stimuli_cmd_dict = {
 'TD_COAP_CORE_01_v01_step_01' :  IUT_CMD + ['-o', 'GET', '-p', 'coap://127.0.0.1:5683/test', ],
 'TD_COAP_CORE_01_v01_step_02' :  IUT_CMD + ['-o', 'GET', '-p', 'coap://127.0.0.1:5683/test', ],
 }
 
-class AmqpListener(threading.Thread):
+class AutomatedIUT(threading.Thread):
 
     def __init__(self, conn):
         threading.Thread.__init__(self)
@@ -67,67 +69,32 @@ class AmqpListener(threading.Thread):
         self.channel.stop_consuming()
 
     def on_request(self, ch, method, props, body):
-        # obj hook so json.loads respects the order of the fields sent -just for visualization purposeses-
-        req_body_dict = json.loads(body.decode('utf-8'),object_pairs_hook=OrderedDict)
-        ch.basic_ack(delivery_tag=method.delivery_tag)
-        logging.info("Message sniffed: %s, body: %s" % (json.dumps(req_body_dict), str(body)))
-        self.message_count += 1
 
-        props_dict={
+
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+        props_dict = {
             'content_type': props.content_type,
-            'content_encoding': props.content_encoding,
-            'headers': props.headers,
             'delivery_mode': props.delivery_mode,
-            'priority': props.priority,
             'correlation_id': props.correlation_id,
             'reply_to': props.reply_to,
-            'expiration': props.expiration,
             'message_id': props.message_id,
             'timestamp': props.timestamp,
             'user_id': props.user_id,
             'app_id': props.app_id,
-            'cluster_id': props.cluster_id,
         }
-        #let's get rid of values which are empty
-        props_dict_only_non_empty_values = {k: v for k, v in props_dict.items() if v is not None}
+        event = Message.from_json(body)
+        event.update_properties(**props_dict)
 
-        print('\n* * * * * * MESSAGE SNIFFED (%s) * * * * * * *'%self.message_count)
-        print("TIME: %s"%datetime.datetime.time(datetime.datetime.now()))
-        print(" - - - ")
-        print("ROUTING_KEY: %s" % method.routing_key)
-        print(" - - - ")
-        print("HEADERS: %s" % props.headers)
-        print(" - - - ")
-        print("PROPS: %s" %json.dumps(props_dict_only_non_empty_values))
-        print(" - - - ")
-        print('BODY %s' % json.dumps(req_body_dict))
-        print(" - - - ")
-        #print("ERRORS: %s" % )
-        print('* * * * * * * * * * * * * * * * * * * * * \n')
+        self.message_count += 1
 
-        if props.content_type != "application/json":
-            print('* * * * * * API VALIDATION WARNING * * * * * * * ')
-            print("props.content_type : " + str(props.content_type))
-            print("application/json was expected")
-            print('* * * * * * * * * * * * * * * * * * * * *  \n')
-
-        if '_type' not in req_body_dict.keys():
-            print('* * * * * * API VALIDATION WARNING * * * * * * * ')
-            print("no < _type > field found")
-            print('* * * * * * * * * * * * * * * * * * * * *  \n')
-
-
-        message = Message.from_json(body.decode('utf-8'))
-
-        print(message)
-
-        if message is None:
+        if event is None:
             return
 
-        if message._type == 'testcoordination.step.execute':
-            if message.step_id in coap_client_tc_stimuli_commands:
-                self._execute_stimuli( message.step_id , coap_client_tc_stimuli_commands[message.step_id] )
-                FINISH and thest the command list
+        elif isinstance(event,MsgStepExecute):
+            if event.step_id in stimuli_cmd_dict:
+                self._execute_stimuli( event.step_id , stimuli_cmd_dict[event.step_id] )
+
 
     def _execute_stimuli(self, stimuli_step_id, cmd):
         logging.info('spawning process with : %s' %cmd)
@@ -140,20 +107,9 @@ class AmqpListener(threading.Thread):
         logging.info('%s executed' % stimuli_step_id)
         logging.info('process stdout: %s' % output)
 
-        self._publish_event(MsgStimuliExecuted())
+        publish_message(self.channel, MsgStimuliExecuted())
 
-    def _publish_event(self, message):
 
-        properties = pika.BasicProperties(
-                content_type='application/json',
-        )
-
-        channel.basic_publish(
-                exchange=AMQP_EXCHANGE,
-                routing_key=message.routing_key,
-                properties=properties,
-                body=message.to_json(),
-        )
 
     def run(self):
         print("Starting thread listening on the event bus")
@@ -180,7 +136,7 @@ if __name__ == '__main__':
 
 
     # start amqp listener thread
-    amqp_listener = AmqpListener(connection)
+    amqp_listener = AutomatedIUT(connection)
     amqp_listener.start()
 
     amqp_listener.join()
