51a283bdd66beffbdf70758a53806cd81231cb3a
Federico Sismondi <federicosismondi@gmail.com>
2017-05-19 09:32:59 +0200

    added sh script for creating the docker images, and keeping track of all automated iut implementations in the repo

---
+++ build_all_docker_images.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+"""
+docker images naming must follow the following conventions:
+
+resource_type-sub_type-resource_name-version
+
+resource_type, sub_type and resource_name cannot contain any special character, nor  '-'
+version format must comply to vx.x
+
+examples:
+
+automated_iut-coap_client-coapthon-v0.1
+automated_iut-coap_server-californium-v0.1
+testing_tool-performance-coap-v0.1
+testing_tool-interoperability-coap-v0.1
+testing_tool-conformance-coap-v0.1
+testing_tool-conformance-6tisch-v0.1
+"""
+
+docker build -t automated_iut-coap_server-californium-v0.1 -f automated_IUTs/coap_server_californium/Dockerfile .
+docker build -t automated_iut-coap_client-coapthon-v0.1 -f automated_IUTs/coap_client_coapthon/Dockerfile .
+docker build -t testing_tool-interoperability-coap-v-0.5 .
