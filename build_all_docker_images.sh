8997db00293396f12684eca29b41ff4eb73ec8a2
Federico Sismondi <federicosismondi@gmail.com>
2017-06-01 15:49:41 +0200

    for builing all necesary docker images compliant with what orchestrator expects

--- build_all_docker_images.sh
+++ build_all_docker_images.sh
@@ -15,8 +15,28 @@ testing_tool-performance-coap-v0.1
 testing_tool-interoperability-coap-v0.1
 testing_tool-conformance-coap-v0.1
 testing_tool-conformance-6tisch-v0.1
+reference_iut-coap_client-coapthon-v0.1
+reference_iut-coap_client (alias)
+reference_iut-coap_server-californium-v0.1
+reference_iut-coap_server (alias)
 """
 
+# let's build the three 3 images used by F-Interop platform
 docker build -t automated_iut-coap_server-californium-v0.1 -f automated_IUTs/coap_server_californium/Dockerfile .
 docker build -t automated_iut-coap_client-coapthon-v0.1 -f automated_IUTs/coap_client_coapthon/Dockerfile .
 docker build -t testing_tool-interoperability-coap-v-0.5 .
+
+# the testing tool for interop and conformance is the same
+docker tag testing_tool-interoperability-coap-v-0.5:latest testing_tool-conformance-coap-v-0.5
+
+# reference iut for coap server is automated_iut-coap_server-californium-v0.1
+docker tag automated_iut-coap_server-californium-v0.1:latest reference_iut-coap_server-californium-v0.1
+# which can be run also by the name of reference_iut-coap_server
+docker tag automated_iut-coap_server-californium-v0.1:latest reference_iut-coap_server
+
+# reference iut for coap client:
+docker tag automated_iut-coap_client-coapthon-v0.1:latest reference_iut-coap_client-coapthon-v0.1
+# which can be run also by the name of reference_iut-coap_client
+docker tag automated_iut-coap_client-coapthon-v0.1:latest reference_iut-coap_client
+
+
