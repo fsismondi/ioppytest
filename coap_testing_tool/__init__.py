f97aa2f4bb3aafc117c1e868495b79e028bdd436
Federico Sismondi <federicosismondi@gmail.com>
2017-08-21 17:18:22 +0200

    added 6lowpan TD for FORMAT spec and validation test

--- coap_testing_tool/__init__.py
+++ coap_testing_tool/__init__.py
@@ -1,9 +1,9 @@
-
 # -*- coding: utf-8 -*-
 
 import os
 import json
-try: # py2 and py3 compat
+
+try:  # py2 and py3 compat
     from urllib.parse import urlparse
 except ImportError:
     from urlparse import urlparse
@@ -12,8 +12,8 @@ __version__ = (0, 0, 6)
 
 project_dir = os.path.abspath(os.path.join(os.path.realpath(__file__), os.pardir))
 if '/coap_testing_tool' in project_dir:
-     project_dir = os.path.abspath(os.path.join(project_dir, os.pardir))
-print('Project dir: %s'%project_dir)
+    project_dir = os.path.abspath(os.path.join(project_dir, os.pardir))
+print('Project dir: %s' % project_dir)
 
 
 def get_from_environment(variable, default):
@@ -26,17 +26,17 @@ def get_from_environment(variable, default):
     return v
 
 
-
 # # # # # # hard variables # # # # # # # # # #
 
-TMPDIR = os.path.join( project_dir,'tmp')
-DATADIR = os.path.join( project_dir,'data')
-RESULTS_DIR = os.path.join( DATADIR,'results')
-PCAP_DIR =  os.path.join( DATADIR,'dumps')
-LOGDIR = os.path.join( project_dir,'log')
-TD_DIR = os.path.join( project_dir,'coap_testing_tool','extended_test_descriptions')
-TD_COAP = os.path.join(TD_DIR,"TD_COAP_CORE.yaml")
-TD_COAP_CFG = os.path.join(TD_DIR,"TD_COAP_CFG.yaml")
+TMPDIR = os.path.join(project_dir, 'tmp')
+DATADIR = os.path.join(project_dir, 'data')
+RESULTS_DIR = os.path.join(DATADIR, 'results')
+PCAP_DIR = os.path.join(DATADIR, 'dumps')
+LOGDIR = os.path.join(project_dir, 'log')
+TD_DIR = os.path.join(project_dir, 'coap_testing_tool', 'extended_test_descriptions')
+TD_COAP = os.path.join(TD_DIR, "TD_COAP_CORE.yaml")
+TD_COAP_CFG = os.path.join(TD_DIR, "TD_COAP_CFG.yaml")
+TD_6LOWPAN_FORMAT = os.path.join(TD_DIR, "TD_6LOWPAN_FORMAT.yaml")
 
 # # # # # # ENV variables # # # # # # # # # #
 
@@ -69,15 +69,14 @@ except KeyError as e:
     AMQP_URL = "amqp://{0}:{1}@{2}/{3}".format(AMQP_USER, AMQP_PASS, AMQP_SERVER, AMQP_VHOST)
 
 print(json.dumps(
-                {
-                    'server': AMQP_SERVER,
-                    'session': AMQP_VHOST,
-                    'user': AMQP_USER,
-                    'pass': '#' * len(AMQP_PASS),
-                    'exchange': AMQP_EXCHANGE
-                }
+    {
+        'server': AMQP_SERVER,
+        'session': AMQP_VHOST,
+        'user': AMQP_USER,
+        'pass': '#' * len(AMQP_PASS),
+        'exchange': AMQP_EXCHANGE
+    }
 ))
-
 
 # # # # # # variables coming from index.json # # # # # # # # # #
 
@@ -105,4 +104,3 @@ __all__ = [
     AGENT_TT_ID,
     INTERACTIVE_SESSION
 ]
-
--- coap_testing_tool/extended_test_descriptions/tests/tests.py
+++ coap_testing_tool/extended_test_descriptions/tests/tests.py
@@ -1,7 +1,6 @@
-from coap_testing_tool import TD_DIR, TD_COAP, TD_COAP_CFG
+from coap_testing_tool import TD_DIR, TD_COAP, TD_COAP_CFG, TD_6LOWPAN_FORMAT
 from coap_testing_tool.test_coordinator.coordinator import import_teds
-from collections import OrderedDict
-import json, unittest, os
+import unittest
 
 """
 python3 -m  pytest coap_testing_tool/extended_test_descriptions/tests/tests.py
@@ -36,6 +35,34 @@ class ImportYamlInteropTestCases(unittest.TestCase):
             assert tc_config.nodes
             assert tc_config.topology
             assert tc_config.description
+
+    def test_yaml_testcase_syntax_6lowpan(self):
+        imported_tcs = import_teds(TD_6LOWPAN_FORMAT)
+        for tc in imported_tcs:
+            print(tc)
+            assert tc.id
+            assert tc.uri
+            assert tc.objective
+            assert tc.configuration_id
+            assert tc.references
+            assert tc.pre_conditions
+            assert tc.sequence
+
+            for step in tc.sequence:
+                print(step)
+                assert step.id
+                assert step.type
+                assert step.description
+
+    # def test_yaml_testcase_configuration_syntax_6lowpan(self):
+    #     imported_configs = import_teds(TD_6LOWPAN_CFG)
+    #     for tc_config in imported_configs:
+    #         print(tc_config)
+    #         assert tc_config.id
+    #         assert tc_config.uri
+    #         assert tc_config.nodes
+    #         assert tc_config.topology
+    #         assert tc_config.description
 
 
 if __name__ == '__main__':
