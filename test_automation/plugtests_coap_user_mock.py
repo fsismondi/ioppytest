971b61b1240b29a09f4ca8abdb050ff6d8c430e6
Federico Sismondi <federicosismondi@gmail.com>
2017-10-06 16:04:46 +0200

    updated lib to last commit and some other minor changes

--- test_automation/plugtests_coap_user_mock.py
+++ test_automation/plugtests_coap_user_mock.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     connection = pika.BlockingConnection(pika.URLParameters(AMQP_URL))
 
     # e.g. for TD COAP CORE from 1 to 31
-    tc_list = ['TD_COAP_CORE_%02d' % tc for tc in range(1, 31)]
+    tc_list = ['TD_COAP_CORE_%02d' % tc for tc in range(1, 3)]
 
     u = UserMock(connection, tc_list)
     u.start()
