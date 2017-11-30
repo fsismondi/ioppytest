89e6f298d15c1f0d145081a69ed8b70ca2aa284b
Federico Sismondi <federicosismondi@gmail.com>
2017-11-30 10:03:50 +0100

    fixed v4 and v6 webserver support

--- ioppytest/webserver/__main__.py
+++ ioppytest/webserver/__main__.py
@@ -6,24 +6,28 @@ COMPONENT_ID = 'webserver'
 
 logger = logging.getLogger(COMPONENT_ID)
 
+
+class HTTPServerV6(HTTPServer):
+    address_family = socket.AF_INET6
+
+
 def launchHttpServer():
     logging.info('starting server...')
     # Server settings
     server_address = ('127.0.0.1', 8080)
     http_serv = HTTPServer(server_address, SimpleHTTPRequestHandler)
-    logging.info('running server on IPv4, host %s '%server_address)
+    logging.info('running server on IPv4, host %s ' % str(server_address))
     http_serv.serve_forever()
 
-class HTTPServerV6(HTTPServer):
-  address_family = socket.AF_INET6
 
 def launchHttpServerV6():
     logging.info('starting server...')
     # Server settings
-    server_address = ('127.0.0.1', 8080)
+    server_address = ('::1', 8080)
     http_serv = HTTPServerV6(server_address, SimpleHTTPRequestHandler)
-    logging.info('running server on IPv6, host %s '%server_address)
+    logging.info('running server on IPv6, host %s ' % str(server_address))
     http_serv.serve_forever()
+
 
 if __name__ == '__main__':
     launchHttpServer()
