b4ce75a844638f1c1d12565d06ce6a1f08e3a030
BhzErlb <erwan.leflem@etudiant.univ-rennes1.fr>
2018-07-12 10:45:51 +0200

    Unit test for l3 reachability test

---
+++ tests/test_automation.py
@@ -0,0 +1,22 @@
+import logging
+import pprint
+import unittest
+from automated_IUTs.automation import AutomatedIUT
+
+class TestAutomation(unittest.TestCase):
+    """
+    python3 -m pytest tests/tests.test_automation.py -vvv
+    """
+
+    def test_check_l3_reachability(self):
+        ip = "127.0.0.1" 
+        reachable = AutomatedIUT.test_l3_reachability(ip)
+        assert reachable
+
+        ip = "::1"
+        reachable = AutomatedIUT.test_l3_reachability(ip)
+        assert reachable
+
+        ip = "192.0.2.1" # Should never be reachable, see RFC 5737
+        reachable = AutomatedIUT.test_l3_reachability(ip)
+        assert not reachable
