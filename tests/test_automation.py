41198db54572101689112d9f26678012a3c053b8
BhzErlb <erwan.leflem@etudiant.univ-rennes1.fr>
2018-07-12 16:34:14 +0200

    Fix for test failling if OS does not support IPV6

--- tests/test_automation.py
+++ tests/test_automation.py
@@ -9,14 +9,19 @@ class TestAutomation(unittest.TestCase):
     """
 
     def test_check_l3_reachability(self):
-        ip = "127.0.0.1"
-        reachable = AutomatedIUT.test_l3_reachability(ip)
-        assert reachable
 
-        ip = "::1"
+        ip = "127.0.0.1"
         reachable = AutomatedIUT.test_l3_reachability(ip)
         assert reachable
 
         ip = "192.0.2.1" # Should never be reachable, see RFC 5737
         reachable = AutomatedIUT.test_l3_reachability(ip)
         assert not reachable
+
+        try:
+           s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+        except:
+           return #this test cannot be executed cause OS doesnt support IPV6
+        ip = "::1"
+        reachable = AutomatedIUT.test_l3_reachability(ip)
+        assert reachable
